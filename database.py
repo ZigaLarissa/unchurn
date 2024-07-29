feae59b332ea69e27897a19edafa13dd7e879d57
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-29 13:32:33 +0200

    database connection

---
+++ database.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+# database_url = 'mongodb+srv://lbizimungu:S9kmYKTmunyDNjNM@cluster0.gaeuej0.mongodb.net/'
+database_url = 'mongodb+srv://lbizimungu:S9kmYKTmunyDNjNM@cluster0.gaeuej0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+client = MongoClient(database_url)
+Database = client['unchurn']
+
+def get_database():
+    return Database
