4ed05f5450fad1be4dbdcb5e37b7bd2c4e29e1b7
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-31 10:29:13 +0200

    ssl certificate on the db

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # database_url = 'mongodb+srv://lbizimungu:S9kmYKTmunyDNjNM@cluster0.gaeuej0.mongodb.net/'
 database_url = 'mongodb+srv://lbizimungu:S9kmYKTmunyDNjNM@cluster0.gaeuej0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-client = MongoClient(database_url, tls=True, tlsAllowInvalidCertificates=True)
+client = MongoClient(database_url)
 Database = client['unchurn']
 
 def get_database():
