e419f719158788ea9b3de52d18a02c3f6caf2cc4
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-31 06:53:10 +0200

    cert_none on db

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # database_url = 'mongodb+srv://lbizimungu:S9kmYKTmunyDNjNM@cluster0.gaeuej0.mongodb.net/'
 database_url = 'mongodb+srv://lbizimungu:S9kmYKTmunyDNjNM@cluster0.gaeuej0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-client = MongoClient(database_url)
+client = MongoClient(database_url, ssl=True, ssl_cert_reqs='CERT_NONE')
 Database = client['unchurn']
 
 def get_database():
